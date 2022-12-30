a38197703fd107d6a08233aa419a2811cc678c87
“Alanis” <sanchezalanis806@gmail.com>
2022-12-29 19:20:38 -0500

    se registro exitosamente todos los archivos

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function encriptar(){
+    const passSistema= "Norevelesmichisme";
+    const msgError= "Incorrecto por sapo";
+    var mensaje = document.getElementById("msg").value;
+    var password = document.getElementById("pass").value;
+    if(passSistema==password){
+        var encriptado = btoa(mensaje);
+        document.getElementById("demo1").innerHTML = encriptado;    
+    }else{
+        document.getElementById("error").innerHTML = msgError;
+    }
+}
+
+function desencriptar(){
+    var mensajeCodigo = document.getElementById("msgCodigo").value;
+    var decrypted = atob(mensajeCodigo);
+    document.getElementById("demo2").innerHTML = decrypted
+}
